2f1d4bbd43fad532d3fb8d1d742ff3d44910d522
Ned Stratton <nedstratton@googlemail.com>
2023-10-09 22:22:29 +0100

    WPF App .NET First Test and simple WPF main window

---
+++ PinpointOnenote/PinpointOnenote/LoginEntry.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PinpointOnenote
+{
+    public class LoginEntry
+    {
+        public int id { get; set; }
+        public LoginTypes LoginType { get; set; }
+        public string LoginDescription { get; set; }
+        public string LoginUrl { get; set; }
+        public string LoginUsername { get; set; }
+        public string LoginPass { get; set; }
+        public bool HasTwoFa { get; set; }
+        public string TwoFaMethod { get; set; }
+        public DateTime LastModified { get; set; }
+        public int LastModifiedSort { get; set; }
+        public int LoginStrength { get; set; }
+    }
+}
---
+++ PinpointOnenote/PinpointOnenote/OnenoteMethods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Microsoft.Office.Interop.OneNote;
+
+namespace PinpointOnenote
+{
+    public static class OnenoteMethods
+    {
+        public static bool IsOnenoteOpen ()
+        {
+            Microsoft.Office.Interop.OneNote.Application app = new Microsoft.Office.Interop.OneNote.Application();
+
+            if (app.Windows.Count < 1)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
