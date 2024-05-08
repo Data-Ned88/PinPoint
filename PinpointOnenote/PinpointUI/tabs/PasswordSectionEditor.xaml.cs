a6579f9cdf251806be74def66090d5d72bd517e7
Ned Stratton <nedstratton@googlemail.com>
2024-05-08 01:47:44 +0100

    8th May 2024 Cont. form wire up Copy single cell and single item editor start.

--- PinpointOnenote/PinpointUI/tabs/PasswordSectionEditor.xaml.cs
+++ PinpointOnenote/PinpointUI/tabs/PasswordSectionEditor.xaml.cs
@@ -46,9 +46,29 @@ namespace PinpointUI.tabs
         private string subBannerText;
         private string passwordBankPageId;
         private XDocument passwordBankPageContent;
+
+
+
         
+        public ICommand CopyCellCommand { get; private set; }
+        private void CopyCell(object parameter) //input param not useful
+        {
+            // Logic to copy cell content to the clipboard
+            int i = existingPasswords.CurrentCell.Column.DisplayIndex;
 
+            TypeConverter converter = TypeDescriptor.GetConverter(typeof(LoginTypes)); // need this to convert Enums to descriptions.
 
+            LoginEntry leClicked = (LoginEntry)existingPasswords.CurrentCell.Item;
+            List<string> gridOrderedProperties = new List<string>
+            {
+                // switch these around in order depending on the column layout of your data grid.
+                leClicked.LoginDescription,
+                converter.ConvertToString(leClicked.LoginType),leClicked.LoginUrl,leClicked.LoginUsername,
+                leClicked.LoginPass,leClicked.HasTwoFa.ToString(),leClicked.TwoFaMethod,leClicked.LoginStrength.Score.ToString()
+            };
+            string valueReturnable = gridOrderedProperties[i];
+            Clipboard.SetText(valueReturnable);
+        }
 
         public event PropertyChangedEventHandler PropertyChanged;
         protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
@@ -137,13 +157,15 @@ namespace PinpointUI.tabs
                 //passwordBankOriginal = LoginFunctionality.HydrateIdAndModifiedSort(passwordBankOriginal);
 
             }
-
-
+            CopyCellCommand = new RelayCommand(CopyCell); // Has to happen before initialise in order to work.
             InitializeComponent();
-
+            
             pwordTabSectionTitle.Text = mainBannerText;
             pwordTabSectionSubTitle.Text = subBannerText;
-
+            setVisibilitySinglePasswordEditorConstants();
+            toggleVisibilitySinglePasswordEditor("new", Visibility.Hidden);
+            toggleVisibilitySinglePasswordEditor("sel", Visibility.Hidden);
+            singleItemAreaHeader.Text = "";
         }
 
         private void PwordTabBackToSections_Click(object sender, RoutedEventArgs e)
@@ -178,9 +200,10 @@ namespace PinpointUI.tabs
             }
             
         }
-        private void setVisibilitySinglePasswordEditor(string set = "new", Visibility _mode = Visibility.Hidden)
+        private void toggleVisibilitySinglePasswordEditor(string set = "new", Visibility _mode = Visibility.Hidden)
         {
-            
+            //This does alternates for NEW Password vs edit existing password
+
             if (set ==  "new")
             {
                 newItemDescInput.Visibility = _mode;
@@ -192,6 +215,8 @@ namespace PinpointUI.tabs
                 newItemTwoFaMethodInput.Visibility = _mode;
                 newItemStrengthInput.Visibility = _mode;
                 btnAddNew.Visibility = _mode;
+                btnCloseNewEditor.Visibility = _mode;
+
             }
             else
             {
@@ -204,33 +229,86 @@ namespace PinpointUI.tabs
                 selItemTwoFaMethodInput.Visibility = _mode;
                 selItemStrengthInput.Visibility = _mode;
                 btnUpdate.Visibility = _mode;
+                btnUndoChanges.Visibility = _mode;
             }
         }
+
+        private void setVisibilitySinglePasswordEditorConstants(Visibility _mode = Visibility.Hidden)
+        {
+            singleItemDescLabel.Visibility = _mode;
+            singleItemTypeLabel.Visibility = _mode;
+            singleItemUrlLabel.Visibility = _mode;
+            singleItemUsernameLabel.Visibility = _mode;
+            singleItemPassPinLabel.Visibility = _mode;
+            singleItemTwoFaLabel.Visibility = _mode;
+            singleItemTwoFaMethodLabel.Visibility = _mode;
+            singleItemStengthLabel.Visibility = _mode;
+            btnPassPinAuto.Visibility = _mode;
+        }
+        private void setVisibilityDeletionButtons(Visibility _mode = Visibility.Hidden)
+        {
+            btnDeleteAll.Visibility = _mode;
+            btnDeleteSelected.Visibility = _mode;
+        }
+
         private void existingPasswords_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+
             if (existingPasswords.SelectedItems.Count == 1)
             {
                 selectedLogin = (LoginEntry)existingPasswords.SelectedItem;
-                setVisibilitySinglePasswordEditor("new", Visibility.Hidden);
-                setVisibilitySinglePasswordEditor("sel", Visibility.Visible);
+                toggleVisibilitySinglePasswordEditor("new", Visibility.Hidden);
+                toggleVisibilitySinglePasswordEditor("sel", Visibility.Visible);
+                setVisibilitySinglePasswordEditorConstants(Visibility.Visible);
+                btnDeleteSelected.Visibility = Visibility.Visible;
+                //singleItemAreaHeader.Text = "Edit Selected Login";
+
 
             }
             else if (existingPasswords.SelectedItems.Count > 1)
             {
-                setVisibilitySinglePasswordEditor("new", Visibility.Hidden);
-                setVisibilitySinglePasswordEditor("sel", Visibility.Hidden);
+                toggleVisibilitySinglePasswordEditor("new", Visibility.Hidden);
+                toggleVisibilitySinglePasswordEditor("sel", Visibility.Hidden);
+                setVisibilitySinglePasswordEditorConstants();
+                btnDeleteSelected.Visibility = Visibility.Visible;
+                singleItemAreaHeader.Text = String.Format("{0} logins selected", existingPasswords.SelectedItems.Count.ToString());
             }
             else
             {
-                //TODO - I don't like this. It hides everything except the labels when no passwords are selected. Hdel the labels as well.??
-                setVisibilitySinglePasswordEditor("new", Visibility.Visible);
-                setVisibilitySinglePasswordEditor("sel", Visibility.Hidden);
+                //TODO - I don't like this. It hides everything except the labels when no passwords are selected. Hide the labels as well.??
+                toggleVisibilitySinglePasswordEditor("new", Visibility.Hidden);
+                toggleVisibilitySinglePasswordEditor("sel", Visibility.Hidden);
+                setVisibilitySinglePasswordEditorConstants();
+                btnDeleteSelected.Visibility = Visibility.Hidden;
+                singleItemAreaHeader.Text = "";
             }
         }
 
+        private void ExistingPasswords_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Subscribe to the event when the data grid is loaded
+            // Ned - we're doing this becuase we can't have edit selected login in the SelectionChanged handler, becuase it will override the save status message from the Update button on existing records.
+            existingPasswords.AddHandler(DataGridRow.MouseLeftButtonUpEvent, new MouseButtonEventHandler(OnRowClicked), true);
+        }
+
+        private void OnRowClicked(object sender, MouseButtonEventArgs e)
+        {
+            if (existingPasswords.SelectedItems.Count == 1)
+            {
+                singleItemAreaHeader.Text = "Edit Selected Login";
+            }
+
+        }
+
+
         private void btnNewPassInExisting_Click(object sender, RoutedEventArgs e)
         {
-
+            toggleVisibilitySinglePasswordEditor("new", Visibility.Visible);
+            toggleVisibilitySinglePasswordEditor("sel", Visibility.Hidden);
+            setVisibilitySinglePasswordEditorConstants(Visibility.Visible);
+            btnDeleteSelected.Visibility = Visibility.Hidden;
+            existingPasswords.SelectedItem = null;
+            singleItemAreaHeader.Text = "New Login";
         }
 
         private void btnImportFromFile_Click(object sender, RoutedEventArgs e)
@@ -245,6 +323,8 @@ namespace PinpointUI.tabs
 
         private void btnUpdate_Click(object sender, RoutedEventArgs e)
         {
+
+
             // THis pushes the update to the Grid, which is read only. It don't want it dynamically updating.
             BindingExpression be = selItemDescInput.GetBindingExpression(TextBox.TextProperty);
             be.UpdateSource();
@@ -263,15 +343,20 @@ namespace PinpointUI.tabs
             be = selItemStrengthInput.GetBindingExpression(TextBox.TextProperty);
             be.UpdateSource();
             existingPasswords.Items.Refresh(); // need to do this to get the Strength scores in the grid to update.
+            
+            singleItemAreaHeader.Text = "Changes saved to selected login.";
 
 
-            //selectedLogin = (LoginEntry)existingPasswords.SelectedItem;
-            //selectedLogin.LoginStrength.Score = 99;
+
         }
 
         private void btnUndoChanges_Click(object sender, RoutedEventArgs e)
         {
-
+            toggleVisibilitySinglePasswordEditor("new", Visibility.Hidden);
+            toggleVisibilitySinglePasswordEditor("sel", Visibility.Hidden);
+            setVisibilitySinglePasswordEditorConstants();
+            btnDeleteSelected.Visibility = Visibility.Hidden;
+            existingPasswords.SelectedItem = null;
         }
 
         private void btnDeleteSelected_Click(object sender, RoutedEventArgs e)
@@ -295,16 +380,28 @@ namespace PinpointUI.tabs
 
         private void btnPassPinAuto_Click(object sender, RoutedEventArgs e)
         {
-            
+            //TODO new modal
         }
 
         private void selItemPassPinInput_TextChanged(object sender, TextChangedEventArgs e)
         {
             // This is clever. When  the value in the pin/password box of the existing item editor is changed, it calculates the logins core for it on the fly so taht the user can preview.
-            LoginStrength lsFly = new LoginStrength((LoginTypes)selItemTypeInput.SelectedItem, selItemPassPinInput.Text, selItemUsernameInput.Text, (bool)selItemTwoFaInput.IsChecked);
-            selItemStrengthInput.Text = lsFly.Score.ToString();
-
+            if (existingPasswords.SelectedItems.Count > 0)
+            {
+                LoginStrength lsFly = new LoginStrength((LoginTypes)selItemTypeInput.SelectedItem, selItemPassPinInput.Text, selItemUsernameInput.Text, (bool)selItemTwoFaInput.IsChecked);
+                selItemStrengthInput.Text = lsFly.Score.ToString();
+            }
             //TODO Make one of these for each input field in the single item editor which affects the login score (Type as ENum, 2FA as bool, user, and password.)
+        }
+
+        private void btnCloseNewEditor_Click(object sender, RoutedEventArgs e)
+        {
+            toggleVisibilitySinglePasswordEditor("new", Visibility.Hidden);
+            toggleVisibilitySinglePasswordEditor("sel", Visibility.Hidden);
+            setVisibilitySinglePasswordEditorConstants();
+            btnDeleteSelected.Visibility = Visibility.Hidden;
+            existingPasswords.SelectedItem = null;
+            singleItemAreaHeader.Text = "";
         }
     }
 }
